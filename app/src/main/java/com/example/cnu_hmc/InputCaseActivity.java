e4120c1ef42ae10c0a4c39b24f40bfa1bfdf3acd
kimjiho-cnu <rlawlgh1027@o.cnu.ac.kr>
2021-10-14 19:21:55 +0900

    inputcase UI

---
+++ app/src/main/java/com/example/cnu_hmc/InputCaseActivity.java
@@ -0,0 +1,89 @@
+package com.example.cnu_hmc;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class InputCaseActivity extends AppCompatActivity {
+
+    Toolbar toolbar_input_case;
+    Spinner spinner_input_case;
+    int spinner_idx, road_case_idx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_case);
+
+        toolbar_input_case = findViewById(R.id.toolbar_input_case);
+        spinner_input_case = findViewById(R.id.spinner_input_case);
+
+        road_case_idx = getIntent().getIntExtra("ROAD_CASE", -1);
+
+        settingActionBar();
+        settingSpinner();
+    }
+
+    private void settingSpinner() {
+        spinner_idx = 0;
+
+        int arrayNumber = 0;
+
+        if (road_case_idx == 0) {
+            arrayNumber = R.array.case_array1;
+        } else if (road_case_idx == 1) {
+            arrayNumber = R.array.case_array2;
+        } else if (road_case_idx == 2) {
+            arrayNumber = R.array.case_array3;
+        } else if (road_case_idx == 3) {
+            arrayNumber = R.array.case_array4;
+        } else if (road_case_idx == 4) {
+            arrayNumber = R.array.case_array5;
+        } else if (road_case_idx == 5) {
+            arrayNumber = R.array.case_array6;
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(InputCaseActivity.this, arrayNumber, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_input_case.setAdapter(adapter);
+        spinner_input_case.setFocusable(true);
+        spinner_input_case.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                spinner_idx = i;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void settingActionBar() {
+        setSupportActionBar(toolbar_input_case);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_chevron_left_solid);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/cnu_hmc/InputRoadActivity.java
+++ app/src/main/java/com/example/cnu_hmc/InputRoadActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class InputRoadActivity extends AppCompatActivity {
 
     Toolbar toolbar_input_road;
+    Button btn_road1, btn_road2, btn_road3, btn_road4, btn_road5, btn_road6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,46 @@ public class InputRoadActivity extends AppCompatActivity {
 
         toolbar_input_road = findViewById(R.id.toolbar_input_road);
         settingActionBar();
+
+        btn_road1 = findViewById(R.id.btn_road1);   // 직선 도로
+        btn_road2 = findViewById(R.id.btn_road2);   // 직선 도로
+        btn_road3 = findViewById(R.id.btn_road3);   // 직선 도로
+        btn_road4 = findViewById(R.id.btn_road4);   // 직선 도로
+        btn_road5 = findViewById(R.id.btn_road5);   // 직선 도로
+        btn_road6 = findViewById(R.id.btn_road6);   // 직선 도로
+
+        btn_road1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 0);
+            startActivity(intent);
+        });
+
+        btn_road2.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 1);
+            startActivity(intent);
+        });
+        btn_road3.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 2);
+            startActivity(intent);
+        });
+        btn_road4.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 3);
+            startActivity(intent);
+        });
+        btn_road5.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 4);
+            startActivity(intent);
+        });
+        btn_road6.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputCaseActivity.class);
+            intent.putExtra("ROAD_CASE", 5);
+            startActivity(intent);
+        });
+
     }
 
     private void settingActionBar() {
